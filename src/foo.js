5878f2c6679700fb637a15688201414b4400d862
jc00ke <jesse@jc00ke.com>
2010-10-10 15:24:50 -0700

    Fixing & adding a basic test

--- src/foo.js
+++ src/foo.js
@@ -5,3 +5,7 @@ app.Person = function() {};
 app.Person.prototype.speak = function(words) {
     return words;
 };
+
+app.Person.prototype.fail = function() {
+    return "Don't you believe in me?";
+};
--- test/foo_test.js
+++ test/foo_test.js
@@ -1,21 +1,14 @@
 module("Person");
 
-test("basic", function() {
-    ok(true, "fucking pass"); // it's not passing :-(
+test("speaks", function() {
+    var p = new app.Person();
+    expect(1);
+    var words = "Hi there";
+    equals(p.speak(words), words, "expect the person to say the words");
 });
 
-//test("speaks" function() {
-    //var p = new app.Person();
-    //expect(1);
-    //var words = "Hi there";
-    //equals(p.speak(words), words, "expect the person to say the words");
-//});
-//PersonTest = TestCase("PersonTest");
-
-//PersonTest.prototype.testSpeak = function() {
-    //var p = new app.Person();
-    //var words = "Hi there";
-    //console.log(p.speak(words));
-    //assertEquals(words, p.speak(words));
-
-//};
+test("fails", function() {
+    var p = new app.Person();
+    expect(1);
+    equals(p.fail(), "Don't you believe in me?", "have faith!");
+});
